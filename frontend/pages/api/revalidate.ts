19ffa40d5d135946f20aee45df9f00a3e61e1b07
Piotr Dymiński <piotrdyminski93@gmail.com>
2022-10-31 20:51:29 +0100

    pass route to revalidate as query param

--- frontend/pages/api/revalidate.ts
+++ frontend/pages/api/revalidate.ts
@@ -7,16 +7,25 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   try {
-    const { model, entry }: StrapiWebhookEntryEvent = req.body;
-    if (model === 'article') {
-      // homepage contains reference to articles, we should revalidate it too
-      await Promise.all([res.revalidate(`/z-zycia-parafii/${entry.slug}`), res.revalidate('/')]);
+    const route = req.query.route as string;
+    if (route) {
+      await res.revalidate(route);
     } else {
-      await res.revalidate(`/${entry.page_slug}`);
+      await handleStrapiWebhook(req, res);
     }
     return res.json({ revalidated: true });
   } catch (err) {
     console.log(err);
     return res.status(500).send('Error revalidating');
+  }
+}
+
+async function handleStrapiWebhook(req: NextApiRequest, res: NextApiResponse) {
+  const { model, entry }: StrapiWebhookEntryEvent = req.body;
+  if (model === 'article') {
+    // homepage contains reference to articles, we should revalidate it too
+    await Promise.all([res.revalidate(`/z-zycia-parafii/${entry.slug}`), res.revalidate('/')]);
+  } else {
+    await res.revalidate(entry.page_slug);
   }
 }
