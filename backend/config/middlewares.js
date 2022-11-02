c73175e65db9c7183d145f0140a1aadb0aa60eef
Zbicku <paweldyminski93@gmail.com>
2022-11-02 23:08:13 +0100

    Added max transfer size limit to strapi `body` middleware (5MB)

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -5,8 +5,18 @@ module.exports = [
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
-  'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: '5mb', // modify form body
+      jsonLimit: '5mb', // modify JSON body
+      textLimit: '5mb', // modify text body
+      formidable: {
+        maxFileSize: 5 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+      },
+    },
+  },
 ];
