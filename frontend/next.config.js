cce6daaeb434c10f8322acaff058cd18fe0886cd
Piotr Dymiński <piotrdyminski93@gmail.com>
2022-11-06 19:49:28 +0100

    add 'pl' locale as default; add site domain to images whitelist

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,10 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
+  i18n: {
+    locales: ['pl'],
+    defaultLocale: 'pl',
+  },
   images: {
     loader: 'default',
     domains: [
       'localhost',
+      'parafiakielcedyminy.pl',
       '139.59.133.110', // DigitalOcean
     ],
   },
