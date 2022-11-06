3aefd03cde7350821dfe843ec06af3f4e943ee3d
Zbicku <paweldyminski93@gmail.com>
2022-11-06 20:12:07 +0100

    Increased max body file size to 20MB.

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -11,11 +11,11 @@ module.exports = [
   {
     name: 'strapi::body',
     config: {
-      formLimit: '5mb', // modify form body
-      jsonLimit: '5mb', // modify JSON body
-      textLimit: '5mb', // modify text body
+      formLimit: '20mb', // modify form body
+      jsonLimit: '20mb', // modify JSON body
+      textLimit: '20mb', // modify text body
       formidable: {
-        maxFileSize: 5 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+        maxFileSize: 20 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
       },
     },
   },
