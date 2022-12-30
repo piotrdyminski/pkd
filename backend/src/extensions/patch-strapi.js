2711c5c9ffa65f04cc2b3579b663e44118610eac
Zbicku <paweldyminski93@gmail.com>
2022-12-30 17:49:33 +0100

    Replacing Strapi library internal files with custom ones to force the polish locale to be a default one (changes to Strapiapp.js and LanguageProvider component files). Custom changes are marked by '#custom-change' comment.

--- backend/src/extensions/patch-strapi.js
+++ backend/src/extensions/patch-strapi.js
@@ -1,5 +1,5 @@
 /*
-  Script for copying the files with custom changes to admin and dashboard panels,
+  Script for copying the files with custom changes to admin, dashboard panels and plugins,
   which is then used by the 'patch-package' to apply it directly in the source code of strapi package.
   For now there is no recommended solution to apply such changes in the strapi v4.
   source: https://forum.strapi.io/t/customize-the-dashboard-welcome-page/939/24
